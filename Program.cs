81e47f16b9b5e96e19691c4a1812641f6c84d25b
Gianni-A <giannialvaradoc@gmail.com>
2017-01-22 23:33:21 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra = "anula la luz azul a la luna";
+            string letraInicio;
+            string letraFinal;
+            char letraTest;
+            
+            int sigueLetra = palabra.Length;
+            for (int i = 0; i <= palabra.Length - 1; i++)
+            {
+                sigueLetra--;
+                letraInicio = palabra.Substring(i, 1);
+                letraTest = palabra[sigueLetra];
+                letraFinal = letraTest.ToString();
+                
+                if (letraInicio == " ")
+                {
+                    i++;
+                    letraInicio = palabra.Substring(i, 1);
+                }
+                if (letraFinal == " ")
+                {
+                    sigueLetra--;
+                    letraTest = palabra[sigueLetra];
+                    letraFinal = letraTest.ToString();
+                }
+
+                if (letraInicio != letraFinal)
+                {
+                    Console.WriteLine("No es un palindromo");
+                    Console.ReadLine();
+                }
+            }
+            
+            Console.WriteLine("Es palindromo");
+
+            Console.ReadLine();
+        }
+    }
+}
